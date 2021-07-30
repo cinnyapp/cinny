c5ae5dfea366e1757a85d629c68491720391a667
Phil Denhoff <phil@denhoff.ca>
2021-07-29 22:54:06 -0700

    Eslint fixes for TSX

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
     'plugin:react/recommended',
+    'eslint:recommended',
     'airbnb',
   ],
   parserOptions: {
@@ -20,5 +22,48 @@ module.exports = {
   rules: {
     'linebreak-style': 0,
     'no-underscore-dangle': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/no-unresolved': 'error',
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never', jsx: 'never', ts: 'never', tsx: 'never',
+    }],
   },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        // always try to resolve types under `<root>@types` directory even it
+        // doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  overrides: [{
+    files: ['**/*.ts', '**/*.tsx'],
+    env: { browser: true, es6: true, node: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: { jsx: true },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      'comma-dangle': ['error', 'always-multiline'],
+      '@typescript-eslint/no-explicit-any': 0,
+    },
+  }],
 };
