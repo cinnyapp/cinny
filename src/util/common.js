3d885ec26275078ca6a0ef5d55a716593db031b3
Ajay Bura <ajbura@gmail.com>
2021-11-06 15:12:36 +0530

    Added debounce, throttle, getUrlParams
    Signed-off-by: Ajay Bura <ajbura@gmail.com>

--- src/util/common.js
+++ src/util/common.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 export function bytesToSize(bytes) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   if (bytes === 0) return 'n/a';
@@ -32,4 +33,54 @@ export function getEventCords(ev) {
 export function abbreviateNumber(number) {
   if (number > 99) return '99+';
   return number;
+}
+
+export class Debounce {
+  constructor() {
+    this.timeoutId = null;
+  }
+
+  /**
+   * @param {function} func - callback function
+   * @param {number} wait - wait in milliseconds to call func
+   * @returns {func} debounceCallback - to pass arguments to func callback
+   */
+  _(func, wait) {
+    const that = this;
+    return function debounceCallback(...args) {
+      clearTimeout(that.timeoutId);
+      that.timeoutId = setTimeout(() => {
+        func.apply(this, args);
+        that.timeoutId = null;
+      }, wait);
+    };
+  }
+}
+
+export class Throttle {
+  constructor() {
+    this.timeoutId = null;
+  }
+
+  /**
+   * @param {function} func - callback function
+   * @param {number} wait - wait in milliseconds to call func
+   * @returns {function} throttleCallback - to pass arguments to func callback
+   */
+  _(func, wait) {
+    const that = this;
+    return function throttleCallback(...args) {
+      if (that.timeoutId !== null) return;
+      that.timeoutId = setTimeout(() => {
+        func.apply(this, args);
+        that.timeoutId = null;
+      }, wait);
+    };
+  }
+}
+
+export function getUrlPrams(paramName) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(paramName);
 }
