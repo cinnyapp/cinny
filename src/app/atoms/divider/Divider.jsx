9454ffd1af911d60c863d7b7f33b721973e5d9d0
Ajay Bura <ajbura@gmail.com>
2021-12-03 18:25:29 +0530

    Update UX of Divider comp
    Signed-off-by: Ajay Bura <ajbura@gmail.com>

--- src/app/atoms/divider/Divider.jsx
+++ src/app/atoms/divider/Divider.jsx
@@ -4,8 +4,8 @@ import './Divider.scss';
 
 import Text from '../text/Text';
 
-function Divider({ text, variant }) {
-  const dividerClass = ` divider--${variant}`;
+function Divider({ text, variant, align }) {
+  const dividerClass = ` divider--${variant} divider--${align}`;
   return (
     <div className={`divider${dividerClass}`}>
       {text !== null && <Text className="divider__text" variant="b3">{text}</Text>}
@@ -16,11 +16,13 @@ function Divider({ text, variant }) {
 Divider.defaultProps = {
   text: null,
   variant: 'surface',
+  align: 'center',
 };
 
 Divider.propTypes = {
   text: PropTypes.string,
-  variant: PropTypes.oneOf(['surface', 'primary', 'caution', 'danger']),
+  variant: PropTypes.oneOf(['surface', 'primary', 'positive', 'caution', 'danger']),
+  align: PropTypes.oneOf(['left', 'center', 'right']),
 };
 
 export default Divider;
