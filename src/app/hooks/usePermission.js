95568bde357fcbff8e83a25dd55301b87c0ab5de
ginnyTheCat <ginnythecat@lelux.net>
2022-01-27 09:52:18 +0100

    Make usePermission work without live permission support

--- src/app/hooks/usePermission.js
+++ src/app/hooks/usePermission.js
@@ -5,19 +5,21 @@ import { useEffect, useState } from 'react';
 export function usePermission(name, initial) {
   const [state, setState] = useState(initial);
 
-  let descriptor;
-
-  const update = () => setState(descriptor.state);
-
   useEffect(() => {
-    navigator.permissions.query({ name }).then((_descriptor) => {
+    let descriptor;
+
+    const update = () => setState(descriptor.state);
+
+    navigator.permissions?.query({ name }).then((_descriptor) => {
       descriptor = _descriptor;
 
       update();
       descriptor.addEventListener('change', update);
     });
 
-    return () => descriptor.removeEventListener('change', update);
+    return () => {
+      if (descriptor) descriptor.removeEventListener('change', update);
+    };
   }, []);
 
   return state;
