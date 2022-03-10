f725713ed406c64158e5fad2913d8766d7720750
ginnyTheCat <ginnythecat@lelux.net>
2022-03-10 23:17:55 +0100

    Lazy load math module

--- src/util/twemojify.jsx
+++ src/util/twemojify.jsx
@@ -1,22 +1,25 @@
 /* eslint-disable import/prefer-default-export */
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import linkifyHtml from 'linkifyjs/html';
 import parse from 'html-react-parser';
 import twemoji from 'twemoji';
-import Math from '../app/atoms/math/Math';
 import { sanitizeText } from './sanitize';
+
+const Math = lazy(() => import('../app/atoms/math/Math'));
 
 const mathOptions = {
   replace: (node) => {
     if (node.attribs?.['data-mx-maths']) {
       return (
-        <Math
-          content={node.attribs['data-mx-maths']}
-          throwOnError={false}
-          errorColor="var(--tc-danger-normal)"
-          displayMode={node.name === 'div'}
-        />
+        <Suspense fallback={<div />}>
+          <Math
+            content={node.attribs['data-mx-maths']}
+            throwOnError={false}
+            errorColor="var(--tc-danger-normal)"
+            displayMode={node.name === 'div'}
+          />
+        </Suspense>
       );
     }
     return null;
