4976c3f60852e504ec98dc41b8d2110092cce8c7
Salamandar <6552989+Salamandar@users.noreply.github.com>
2022-03-31 19:17:23 +0200

    Set "Follow system theme" as default.

--- src/client/state/settings.js
+++ src/client/state/settings.js
@@ -71,8 +71,8 @@ class Settings extends EventEmitter {
     if (typeof this.useSystemTheme === 'boolean') return this.useSystemTheme;
 
     const settings = getSettings();
-    if (settings === null) return false;
-    if (typeof settings.useSystemTheme === 'undefined') return false;
+    if (settings === null) return true;
+    if (typeof settings.useSystemTheme === 'undefined') return true;
     return settings.useSystemTheme;
   }
 
