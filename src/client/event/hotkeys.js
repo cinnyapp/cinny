c864b37f8a1e02bc9662792e72b56b117507391f
dean <dean@dbassett.dev>
2022-05-11 21:16:50 -0700

    pasting should focus the message field
    also refactored a small amount to use KeyEvent.code
    instead of KeyEvent.keyCode, which is deprecated.
    
    fixes ajbura/cinny#544

--- src/client/event/hotkeys.js
+++ src/client/event/hotkeys.js
@@ -3,53 +3,90 @@ import navigation from '../state/navigation';
 import { markAsRead } from '../action/notifications';
 
 function listenKeyboard(event) {
-  // Ctrl/Cmd +
-  if (event.ctrlKey || event.metaKey) {
-    // k - for search Modal
-    if (event.keyCode === 75) {
-      event.preventDefault();
-      if (navigation.isRawModalVisible) return;
-      openSearch();
-    }
-  }
+	// Ctrl/Cmd +
+	if (event.ctrlKey || event.metaKey) {
+		// search modal
+		if (event.code === 'KeyK') {
+			event.preventDefault();
+			if (navigation.isRawModalVisible) {
+				return;
+			}
+			openSearch();
+		}
 
-  if (!event.ctrlKey && !event.altKey) {
-    if (navigation.isRawModalVisible) return;
-    if (['text', 'textarea'].includes(document.activeElement.type)) {
-      return;
-    }
+		// focus message field on paste
+		if (event.code === 'KeyV') {
+			const msgTextarea = document.getElementById('message-textarea');
+			msgTextarea?.focus();
+		}
+	}
 
-    // esc
-    if (event.keyCode === 27) {
-      if (navigation.isRoomSettings) {
-        toggleRoomSettings();
-        return;
-      }
-      if (navigation.selectedRoomId) {
-        markAsRead(navigation.selectedRoomId);
-        return;
-      }
-    }
+	if (!event.ctrlKey && !event.altKey && !event.metaKey) {
+		if (navigation.isRawModalVisible) return;
+		if (['text', 'textarea'].includes(document.activeElement.type)) {
+			return;
+		}
 
-    // Don't allow these keys to type/focus message field
-    if ((event.keyCode !== 8 && event.keyCode < 48)
-      || (event.keyCode >= 91 && event.keyCode <= 93)
-      || (event.keyCode >= 112 && event.keyCode <= 183)) {
-      return;
-    }
+		if (event.code === 'Escape') {
+			if (navigation.isRoomSettings) {
+				toggleRoomSettings();
+				return;
+			}
+			if (navigation.selectedRoomId) {
+				markAsRead(navigation.selectedRoomId);
+				return;
+			}
+		}
 
-    // press any key to focus and type in message field
-    const msgTextarea = document.getElementById('message-textarea');
-    msgTextarea?.focus();
-  }
+		// focus the text field on most keypresses
+		if (shouldFocusMessageField(event.code)) {
+			// press any key to focus and type in message field
+			const msgTextarea = document.getElementById('message-textarea');
+			msgTextarea?.focus();
+		}
+	}
+}
+
+function shouldFocusMessageField(code) {
+
+	// should focus on alphanumeric values, and backspace
+	if (code.startsWith('Key')) {
+		return true;
+	}
+	if (code.startsWith('Digit')) {
+		return true;
+	}
+	if (code === 'Backspace') {
+		return true;
+	}
+
+	// do not focus if super key is pressed
+	if (code.startsWith('Meta')) { // chrome
+		return false;
+	}
+	if (code.startsWith('OS')) { // firefox
+		return false;
+	}
+
+	// do not focus on F keys
+	if (/^F\d+$/.test(code)) {
+		return false;
+	}
+
+	// do not focus on numlock/scroll lock
+	if (code === 'NumLock' || code === 'ScrollLock') {
+		return false;
+	}
+
+	return true;
 }
 
 function initHotkeys() {
-  document.body.addEventListener('keydown', listenKeyboard);
+	document.body.addEventListener('keydown', listenKeyboard);
 }
 
 function removeHotkeys() {
-  document.body.removeEventListener('keydown', listenKeyboard);
+	document.body.removeEventListener('keydown', listenKeyboard);
 }
 
 export { initHotkeys, removeHotkeys };
