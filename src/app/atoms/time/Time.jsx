f30bd94ca2915fcb2c32adf4a361430cb876db05
ginnyTheCat <ginnythecat@lelux.net>
2022-08-05 10:11:37 +0200

    Not always display time

--- src/app/atoms/time/Time.jsx
+++ src/app/atoms/time/Time.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import dateFormat from 'dateformat';
+import { isInSameDay } from '../../../util/common';
 
 function Time({ timestamp }) {
   const date = new Date(timestamp);
+
+  const compareDate = new Date();
+  const isToday = isInSameDay(date, compareDate);
+
+  compareDate.setDate(compareDate.getDate() - 1);
+  const isYesterday = isInSameDay(date, compareDate);
+
+  const formattedDate = dateFormat(date, isToday || isYesterday ? 'hh:MM TT' : 'dd/mm/yyyy');
+
   return (
     <time
       dateTime={date.toISOString()}
       title={dateFormat(date, 'dd mmmm yyyy, hh:MM TT')}
     >
-      {dateFormat(date, 'hh:MM TT')}
+      {isYesterday ? `Yesterday, ${formattedDate}` : formattedDate}
     </time>
   );
 }
