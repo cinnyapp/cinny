6c8434a1cf9ea51befb127d12ba98aeb6e907a5c
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2022-08-22 20:05:06 +0530

    Change font to Public Sans

--- src/font.js
+++ src/font.js
@@ -1,5 +1,6 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource/inter/variable.css';
+import '@fontsource/noto-sans/300.css';
+import '@fontsource/noto-sans/400.css';
+import '@fontsource/noto-sans/500.css';
+import '@fontsource/noto-sans/600.css';
+import '@fontsource/noto-sans/700.css';
+import '@fontsource/public-sans/variable.css';
