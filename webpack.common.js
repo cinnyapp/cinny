7bec76b993283217f0daa35a533fad86ca9647d0
ginnyTheCat <ginnythecat@lelux.net>
2022-09-27 19:56:31 +0200

    Install jest

--- webpack.common.js
+++ webpack.common.js
@@ -28,12 +28,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
+        use: ['babel-loader'], 
       },
       {
         test: /\.html$/,
