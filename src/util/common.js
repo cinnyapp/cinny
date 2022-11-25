27ccc8b6eabc0a16033d89894bff3603a92523e3
Stefano Pigozzi <me@steffo.eu>
2022-11-25 08:57:48 +0000

    Use IEC 80000-13 byte units
    Since cinny is counting bytes as powers of 1024, it should display the binary units of measurment kibibyte, mebibyte, gibibyte, and so on.
    
    Possibly fixes #986.

--- src/util/common.js
+++ src/util/common.js
@@ -1,6 +1,8 @@
 /* eslint-disable max-classes-per-file */
 export function bytesToSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  // IEC 80000-13
+  // https://en.wikipedia.org/wiki/Kilobyte
+  const sizes = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
   if (bytes === 0) return 'n/a';
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
   if (i === 0) return `${bytes} ${sizes[i]}`;
