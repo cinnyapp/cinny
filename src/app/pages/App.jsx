bbdac4d0d568c263dc6368df0075a38df567e352
Ahmed Korani <ahmed.korani@bld.ai>
2022-12-29 00:59:49 +0200

    Add a dummy global event listener

--- src/app/pages/App.jsx
+++ src/app/pages/App.jsx
@@ -6,6 +6,9 @@ import Auth from '../templates/auth/Auth';
 import Client from '../templates/client/Client';
 
 function App() {
+  window.addEventListener('cross-sign', () => {
+    console.log('test the new event listener');
+  });
   return isAuthenticated() ? <Client /> : <Auth />;
 }
 
