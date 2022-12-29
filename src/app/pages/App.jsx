ab6a23041609acec651f9895a9b5ebba51a103ba
Ahmed Korani <ahmed.korani@bld.ai>
2022-12-29 02:55:48 +0200

    Add implementation for cross signing manual trigger through the global event listener

--- src/app/pages/App.jsx
+++ src/app/pages/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import { isAuthenticated } from '../../client/state/auth';
+import { SetupCrossSigningUsingPassPhrase } from '../organisms/settings/CrossSigning';
 
 import Auth from '../templates/auth/Auth';
 import Client from '../templates/client/Client';
 
 function App() {
-  window.addEventListener('cross-sign', () => {
-    console.log('test the new event listener');
+  window.addEventListener('CrossSignEvent', async (event) => {
+    console.log('Cross Signing Event Handler Triggered');
+    const passPhrase = event.detail.passphrase;
+    console.log('Pass phrase is: ', passPhrase);
+    try{
+      await SetupCrossSigningUsingPassPhrase(passPhrase);
+      console.log('#######################');
+      console.log('Verification Successful');
+    } catch (error) {
+      console.log('Verification Failed Error Occured');
+      console.log(error);
+    }
   });
   return isAuthenticated() ? <Client /> : <Auth />;
 }
