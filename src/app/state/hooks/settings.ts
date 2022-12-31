9bd913e1746b0c6eac9c294e96fe8d157072d139
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2022-12-31 19:10:00 +0530

    Extract set settings hook

--- src/app/state/hooks/settings.ts
+++ src/app/state/hooks/settings.ts
@@ -1,15 +1,13 @@
 import { atom, useAtomValue, useSetAtom, WritableAtom } from 'jotai';
+import { SetAtom } from 'jotai/core/atom';
 import { selectAtom } from 'jotai/utils';
 import { useMemo } from 'react';
 import { Settings } from '../settings';
 
-export const useSetting = <K extends keyof Settings>(
+export const useSetSetting = <K extends keyof Settings>(
   settingsAtom: WritableAtom<Settings, Settings>,
   key: K
-): [Settings[K], (value: Settings[K]) => void] => {
-  const selector = useMemo(() => (s: Settings) => s[key], [key]);
-  const setting = useAtomValue(selectAtom(settingsAtom, selector));
-
+) => {
   const setterAtom = useMemo(
     () =>
       atom<null, Settings[K]>(null, (get, set, value) => {
@@ -20,7 +18,17 @@ export const useSetting = <K extends keyof Settings>(
     [settingsAtom, key]
   );
 
-  const setter = useSetAtom(setterAtom);
+  return useSetAtom(setterAtom);
+};
+
+export const useSetting = <K extends keyof Settings>(
+  settingsAtom: WritableAtom<Settings, Settings>,
+  key: K
+): [Settings[K], SetAtom<Settings[K], void>] => {
+  const selector = useMemo(() => (s: Settings) => s[key], [key]);
+  const setting = useAtomValue(selectAtom(settingsAtom, selector));
+
+  const setter = useSetSetting(settingsAtom, key);
 
   return [setting, setter];
 };
