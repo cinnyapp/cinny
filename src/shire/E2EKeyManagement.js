0d06aa4cd8588afa452599be1226f2ad9e406322
Ahmed Korani <ahmed.korani@bld.ai>
2023-01-10 15:56:43 +0200

    Add an import function for the E2EE room keys

--- src/shire/E2EKeyManagement.js
+++ src/shire/E2EKeyManagement.js
@@ -1,5 +1,5 @@
 import initMatrix from '../client/initMatrix';
-import { encryptMegolmKeyFile } from '../util/cryptE2ERoomKeys';
+import { encryptMegolmKeyFile, decryptMegolmKeyFile } from '../util/cryptE2ERoomKeys';
 
 async function getE2ERoomKeys() {
   try {
@@ -16,8 +16,22 @@ async function getE2ERoomKeys() {
     console.log(encKeys.toString());
     return encKeys;
   } catch (e) {
-    return 'error';
     // TODO fill in the catch function
+    return 'error';
+  }
+}
+
+async function importE2EERoomKeys(file, password) {
+  try {
+    const arrayBuffer = await file.arrayBuffer();
+    const keys = await decryptMegolmKeyFile(arrayBuffer, password);
+    console.log(keys);
+    console.log(JSON.parse(keys));
+    await initMatrix.matrixClient.importRoomKeys(JSON.parse(keys));
+    return keys;
+  } catch (e) {
+    // TODO fill in the catch function
+    return 'error';
   }
 }
 
