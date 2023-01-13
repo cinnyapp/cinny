557214e42301da6e7edc6643034f0de8bd800912
Ahmed Korani <ahmed.korani@bld.ai>
2023-01-13 23:53:32 +0200

    Add 2 functions for E2EE keys import and export in shire directory

--- src/shire/E2EKeyManagement.js
+++ src/shire/E2EKeyManagement.js
@@ -1,38 +1,41 @@
 import initMatrix from '../client/initMatrix';
 import { encryptMegolmKeyFile, decryptMegolmKeyFile } from '../util/cryptE2ERoomKeys';
 
-async function getE2ERoomKeys() {
+async function getE2ERoomKeys(encrpytionPassword) {
   try {
     const keys = await initMatrix.matrixClient.exportRoomKeys();
     // TODO ShireApp: change the password parameter with a proper secret key
     // for encryption or just export the keys
-    const encKeys = await encryptMegolmKeyFile(JSON.stringify(keys), 'root');
     console.log('########## keys before encryption');
     console.log(keys);
     console.log(JSON.stringify(keys));
-
+    const encKeys = await encryptMegolmKeyFile(
+      JSON.stringify(keys),
+      encrpytionPassword,
+    );
     console.log('########## keys after encryption');
     console.log(encKeys);
-    console.log(encKeys.toString());
     return encKeys;
   } catch (e) {
     // TODO fill in the catch function
-    return 'error';
+    return 'Exporting E2EE keys process failed';
   }
 }
 
-async function importE2EERoomKeys(file, password) {
+async function importE2EERoomKeys(encryptedKeys, encrpytionPassword) {
   try {
-    const arrayBuffer = await file.arrayBuffer();
-    const keys = await decryptMegolmKeyFile(arrayBuffer, password);
+    console.log('############### Started E2EE keys decryption process ############');
+    console.log(encryptedKeys);
+    const keys = await decryptMegolmKeyFile(encryptedKeys, encrpytionPassword);
+    console.log('############### after decrypt ############');
     console.log(keys);
     console.log(JSON.parse(keys));
-    await initMatrix.matrixClient.importRoomKeys(JSON.parse(keys));
+    await initMatrix.matrixClient.importRoomKeys(keys);
     return keys;
   } catch (e) {
     // TODO fill in the catch function
-    return 'error';
+    return 'Importing E2EE keys process failed';
   }
 }
 
-export default getE2ERoomKeys;
+export { importE2EERoomKeys, getE2ERoomKeys };
