b391f21d5f656c3332946f781fb343d8c25736c9
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-01-29 10:36:32 +0530

    remove unused polyfill

--- vite.config.js
+++ vite.config.js
@@ -29,36 +29,35 @@ const copyFiles = {
 
 const nodePolyfillAliases = {
   util: 'rollup-plugin-node-polyfills/polyfills/util',
-  sys: 'util',
+  // sys: 'util',
   // events: 'rollup-plugin-node-polyfills/polyfills/events',
   stream: 'rollup-plugin-node-polyfills/polyfills/stream',
   path: 'rollup-plugin-node-polyfills/polyfills/path',
   querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
-  punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+  // punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
   url: 'rollup-plugin-node-polyfills/polyfills/url',
-  string_decoder:
-      'rollup-plugin-node-polyfills/polyfills/string-decoder',
-  http: 'rollup-plugin-node-polyfills/polyfills/http',
-  https: 'rollup-plugin-node-polyfills/polyfills/http',
-  os: 'rollup-plugin-node-polyfills/polyfills/os',
+  // string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder',
+  // http: 'rollup-plugin-node-polyfills/polyfills/http',
+  // https: 'rollup-plugin-node-polyfills/polyfills/http',
+  // os: 'rollup-plugin-node-polyfills/polyfills/os',
   assert: 'rollup-plugin-node-polyfills/polyfills/assert',
-  constants: 'rollup-plugin-node-polyfills/polyfills/constants',
-  _stream_duplex:
-      'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
-  _stream_passthrough:
-      'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
-  _stream_readable:
-      'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
-  _stream_writable:
-      'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
-  _stream_transform:
-      'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
-  timers: 'rollup-plugin-node-polyfills/polyfills/timers',
-  console: 'rollup-plugin-node-polyfills/polyfills/console',
-  vm: 'rollup-plugin-node-polyfills/polyfills/vm',
-  zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
-  tty: 'rollup-plugin-node-polyfills/polyfills/tty',
-  domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+  // constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+  // _stream_duplex:
+  //     'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+  // _stream_passthrough:
+  //     'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+  // _stream_readable:
+  //     'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+  // _stream_writable:
+  //     'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+  // _stream_transform:
+  //     'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+  // timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+  // console: 'rollup-plugin-node-polyfills/polyfills/console',
+  // vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+  // zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+  // tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+  // domain: 'rollup-plugin-node-polyfills/polyfills/domain',
   buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
   process: 'rollup-plugin-node-polyfills/polyfills/process-es6'
 }
