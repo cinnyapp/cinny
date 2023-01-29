b538351cfe5e21daeba436509dfa69f42f8f3874
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-01-29 10:24:03 +0530

    Add node polyfills

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { wasm } from '@rollup/plugin-wasm';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 import { svgLoader } from './viteSvgLoader';
 
 const copyFiles = {
@@ -25,6 +27,42 @@ const copyFiles = {
   ],
 }
 
+const nodePolyfillAliases = {
+  util: 'rollup-plugin-node-polyfills/polyfills/util',
+  sys: 'util',
+  // events: 'rollup-plugin-node-polyfills/polyfills/events',
+  stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+  path: 'rollup-plugin-node-polyfills/polyfills/path',
+  querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+  punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+  url: 'rollup-plugin-node-polyfills/polyfills/url',
+  string_decoder:
+      'rollup-plugin-node-polyfills/polyfills/string-decoder',
+  http: 'rollup-plugin-node-polyfills/polyfills/http',
+  https: 'rollup-plugin-node-polyfills/polyfills/http',
+  os: 'rollup-plugin-node-polyfills/polyfills/os',
+  assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+  constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+  _stream_duplex:
+      'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+  _stream_passthrough:
+      'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+  _stream_readable:
+      'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+  _stream_writable:
+      'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+  _stream_transform:
+      'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+  timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+  console: 'rollup-plugin-node-polyfills/polyfills/console',
+  vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+  zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+  tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+  domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+  buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+  process: 'rollup-plugin-node-polyfills/polyfills/process-es6'
+}
+
 export default defineConfig({
   appType: 'spa',
   publicDir: false,
@@ -38,9 +76,33 @@ export default defineConfig({
     wasm(),
     react(),
   ],
+  resolve: {
+    alias: nodePolyfillAliases
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+        define: {
+          global: 'globalThis'
+        },
+        plugins: [
+          // Enable esbuild polyfill plugins
+          NodeGlobalsPolyfillPlugin({
+            process: false,
+            buffer: true,
+          }),
+        ]
+    }
+},
   build: {
     outDir: 'dist',
     sourcemap: true,
     copyPublicDir: false,
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill({ crypto: true }),
+      ]
+    }
   },
 });
