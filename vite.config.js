374d2d1962c92cb75c747c7cfa1d86f2fd7ef736
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-01-30 15:20:53 +1100

    Add node polyfills (#1093)
    * Add node polyfills
    
    * remove unused polyfill
    
    * enable fs in node polyfill
    
    * remove polyfill aliases and crypto
    
    * Fix build error
    
    * Fix buffer injection

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { wasm } from '@rollup/plugin-wasm';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import inject from '@rollup/plugin-inject';
 import { svgLoader } from './viteSvgLoader';
 
 const copyFiles = {
@@ -39,9 +41,28 @@ export default defineConfig({
     wasm(),
     react(),
   ],
+  optimizeDeps: {
+    esbuildOptions: {
+        define: {
+          global: 'globalThis'
+        },
+        plugins: [
+          // Enable esbuild polyfill plugins
+          NodeGlobalsPolyfillPlugin({
+            process: false,
+            buffer: true,
+          }),
+        ]
+    }
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
     copyPublicDir: false,
+    rollupOptions: {
+      plugins: [
+        inject({ Buffer: ['buffer', 'Buffer'] })
+      ]
+    }
   },
 });
