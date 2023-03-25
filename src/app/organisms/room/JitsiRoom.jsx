cbfeae60d6d1defc3388a132d7c7f3ef4cfef852
IceDBorn <github.envenomed@dralias.com>
2023-03-26 01:08:48 +0200

    Remove unused import

--- src/app/organisms/room/JitsiRoom.jsx
+++ src/app/organisms/room/JitsiRoom.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './JitsiRoom.scss';
 import { JitsiMeeting } from '@jitsi/react-sdk';
 import Draggable from 'react-draggable';
