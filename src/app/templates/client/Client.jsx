4dcfaec9303b2231ee444e7284d4f58ae320dcaf
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-04-14 09:23:59 +0530

    Add matrix client context

--- src/app/templates/client/Client.jsx
+++ src/app/templates/client/Client.jsx
@@ -21,6 +21,7 @@ import cons from '../../../client/state/cons';
 import DragDrop from '../../organisms/drag-drop/DragDrop';
 
 import VerticalMenuIC from '../../../../public/res/ic/outlined/vertical-menu.svg';
+import { MatrixClientProvider } from '../../hooks/useMatrixClient';
 
 function Client() {
   const [isLoading, changeLoading] = useState(true);
@@ -44,19 +45,16 @@ function Client() {
     navigation.on(cons.events.navigation.ROOM_SELECTED, onRoomSelected);
     navigation.on(cons.events.navigation.NAVIGATION_OPENED, onNavigationSelected);
 
-    return (() => {
+    return () => {
       navigation.removeListener(cons.events.navigation.ROOM_SELECTED, onRoomSelected);
       navigation.removeListener(cons.events.navigation.NAVIGATION_OPENED, onNavigationSelected);
-    });
+    };
   }, []);
 
   useEffect(() => {
     let counter = 0;
     const iId = setInterval(() => {
-      const msgList = [
-        'Almost there...',
-        'Looks like you have a lot of stuff to heat up!',
-      ];
+      const msgList = ['Almost there...', 'Looks like you have a lot of stuff to heat up!'];
       if (counter === msgList.length - 1) {
         setLoadingMsg(msgList[msgList.length - 1]);
         clearInterval(iId);
@@ -80,22 +78,28 @@ function Client() {
         <div className="loading__menu">
           <ContextMenu
             placement="bottom"
-            content={(
+            content={
               <>
                 <MenuItem onClick={() => initMatrix.clearCacheAndReload()}>
                   Clear cache & reload
                 </MenuItem>
                 <MenuItem onClick={() => initMatrix.logout()}>Logout</MenuItem>
               </>
+            }
+            render={(toggle) => (
+              <IconButton size="extra-small" onClick={toggle} src={VerticalMenuIC} />
             )}
-            render={(toggle) => <IconButton size="extra-small" onClick={toggle} src={VerticalMenuIC} />}
           />
         </div>
         <Spinner />
-        <Text className="loading__message" variant="b2">{loadingMsg}</Text>
+        <Text className="loading__message" variant="b2">
+          {loadingMsg}
+        </Text>
 
         <div className="loading__appname">
-          <Text variant="h2" weight="medium">Cinny</Text>
+          <Text variant="h2" weight="medium">
+            Cinny
+          </Text>
         </div>
       </div>
     );
@@ -158,25 +162,27 @@ function Client() {
   }
 
   return (
-    <div
-      className="client-container"
-      onDragOver={handleDragOver}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onDrop={handleDrop}
-    >
-      <div className="navigation__wrapper" ref={navWrapperRef}>
-        <Navigation />
+    <MatrixClientProvider value={initMatrix.matrixClient}>
+      <div
+        className="client-container"
+        onDragOver={handleDragOver}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        <div className="navigation__wrapper" ref={navWrapperRef}>
+          <Navigation />
+        </div>
+        <div className={`room__wrapper ${classNameHidden}`} ref={roomWrapperRef}>
+          <Room />
+        </div>
+        <Windows />
+        <Dialogs />
+        <EmojiBoardOpener />
+        <ReusableContextMenu />
+        <DragDrop isOpen={dragCounter !== 0} />
       </div>
-      <div className={`room__wrapper ${classNameHidden}`} ref={roomWrapperRef}>
-        <Room />
-      </div>
-      <Windows />
-      <Dialogs />
-      <EmojiBoardOpener />
-      <ReusableContextMenu />
-      <DragDrop isOpen={dragCounter !== 0} />
-    </div>
+    </MatrixClientProvider>
   );
 }
 
