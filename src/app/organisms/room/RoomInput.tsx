202ccfb2f4444c0ee35fc4e22266eb270aa76ca1
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-05-09 21:32:08 +0530

    add emojiboard in room input

--- src/app/organisms/room/RoomInput.tsx
+++ src/app/organisms/room/RoomInput.tsx
@@ -3,7 +3,7 @@ import isHotkey from 'is-hotkey';
 import { MsgType } from 'matrix-js-sdk';
 import { ReactEditor } from 'slate-react';
 
-import { Icon, IconButton, Icons, Line } from 'folds';
+import { Icon, IconButton, Icons, Line, PopOut } from 'folds';
 import { useMatrixClient } from '../../hooks/useMatrixClient';
 import {
   CustomEditor,
@@ -21,6 +21,8 @@ import {
   RoomMentionAutocomplete,
   UserMentionAutocomplete,
 } from '../../components/editor';
+import { EmojiBoard } from '../../components/emoji-board';
+import { UseStateProvider } from '../../components/UseStateProvider';
 
 interface RoomInputProps {
   roomId: string;
@@ -70,6 +72,10 @@ export function RoomInput({ roomId }: RoomInputProps) {
     setAutocompleteQuery(query);
   };
 
+  const handleEmojiSelect = (unicode: string) => {
+    editor.insertText(unicode);
+  };
+
   return (
     <div>
       {autocompleteQuery?.prefix === AutocompletePrefix.RoomMention && (
@@ -109,9 +115,39 @@ export function RoomInput({ roomId }: RoomInputProps) {
             >
               <Icon src={toolbar ? Icons.AlphabetUnderline : Icons.Alphabet} />
             </IconButton>
-            <IconButton variant="SurfaceVariant" size="300" radii="300">
-              <Icon src={Icons.Smile} />
-            </IconButton>
+            <UseStateProvider initial={false}>
+              {(emojiBoard, setEmojiBoard) => (
+                <PopOut
+                  offset={16}
+                  position="top"
+                  align="end"
+                  open={emojiBoard}
+                  content={
+                    <EmojiBoard
+                      returnFocusOnDeactivate={false}
+                      onEmojiSelect={handleEmojiSelect}
+                      requestClose={() => {
+                        setEmojiBoard(false);
+                        ReactEditor.focus(editor);
+                      }}
+                    />
+                  }
+                >
+                  {(anchorRef) => (
+                    <IconButton
+                      aria-pressed={emojiBoard}
+                      ref={anchorRef}
+                      onClick={() => setEmojiBoard(!emojiBoard)}
+                      variant="SurfaceVariant"
+                      size="300"
+                      radii="300"
+                    >
+                      <Icon src={Icons.Smile} />
+                    </IconButton>
+                  )}
+                </PopOut>
+              )}
+            </UseStateProvider>
             <IconButton onClick={submit} variant="SurfaceVariant" size="300" radii="300">
               <Icon src={Icons.Send} />
             </IconButton>
