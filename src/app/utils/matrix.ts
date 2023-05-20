ea05708e8e1f60e3fc32db2df66e78c5a62178da
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-05-20 16:57:26 +0530

    add getImageInfo to matrix utils

--- src/app/utils/matrix.ts
+++ src/app/utils/matrix.ts
@@ -1,3 +1,5 @@
+import { IImageInfo } from 'matrix-js-sdk';
+
 export const matchMxId = (id: string): RegExpMatchArray | null =>
   id.match(/^([@!$+#])(\S+):(\S+)$/);
 
@@ -8,3 +10,21 @@ export const getMxIdServer = (userId: string): string | undefined => matchMxId(u
 export const getMxIdLocalPart = (userId: string): string | undefined => matchMxId(userId)?.[2];
 
 export const isUserId = (id: string): boolean => validMxId(id) && id.startsWith('@');
+
+export const getImageInfo = async (imageUrl: string): Promise<IImageInfo> => {
+  const img = document.createElement('img');
+  img.src = imageUrl;
+
+  const res = await fetch(imageUrl);
+  const blob = await res.blob();
+
+  const info: IImageInfo = {};
+
+  info.w = img.width;
+  info.h = img.height;
+  info.mimetype = blob.type;
+  info.size = blob.size;
+  info.thumbnail_info = { ...info };
+
+  return info;
+};
