67700a7b916c6848c0bd5f0afb0acf3ab4dac0ff
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-06-05 16:11:52 +0530

    add common utils

--- src/app/utils/common.ts
+++ src/app/utils/common.ts
@@ -1,3 +1,5 @@
+import { IconName, IconSrc } from 'folds';
+
 export const bytesToSize = (bytes: number): string => {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   if (bytes === 0) return '0KB';
@@ -8,3 +10,23 @@ export const bytesToSize = (bytes: number): string => {
 
   return `${(bytes / 1000 ** sizeIndex).toFixed(1)} ${sizes[sizeIndex]}`;
 };
+
+export const getFileTypeIcon = (icons: Record<IconName, IconSrc>, fileType: string): IconSrc => {
+  const type = fileType.toLowerCase();
+  if (type.startsWith('audio')) {
+    return icons.Play;
+  }
+  if (type.startsWith('video')) {
+    return icons.Vlc;
+  }
+  if (type.startsWith('image')) {
+    return icons.Photo;
+  }
+  return icons.File;
+};
+
+export const fulfilledPromiseSettledResult = <T>(prs: PromiseSettledResult<T>[]): T[] =>
+  prs.reduce<T[]>((values, pr) => {
+    if (pr.status === 'fulfilled') values.push(pr.value);
+    return values;
+  }, []);
