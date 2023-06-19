430e144349811177c7938508309e8010080d6171
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-06-19 15:09:34 +0530

    add filters for members

--- src/app/organisms/room/MembersDrawer.tsx
+++ src/app/organisms/room/MembersDrawer.tsx
@@ -1,22 +1,27 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import {
   Avatar,
   AvatarFallback,
   AvatarImage,
   Box,
   Chip,
+  ContainerColor,
   Header,
   Icon,
   IconButton,
   Icons,
   Input,
+  Menu,
+  MenuItem,
+  PopOut,
   Scroll,
   Text,
   color,
   config,
 } from 'folds';
-import { Room } from 'matrix-js-sdk';
+import { Room, RoomMember } from 'matrix-js-sdk';
 import { useVirtualizer } from '@tanstack/react-virtual';
+import FocusTrap from 'focus-trap-react';
 import millify from 'millify';
 
 import * as css from './MembersDrawer.css';
@@ -26,6 +31,106 @@ import {
   getIntersectionObserverEntry,
   useIntersectionObserver,
 } from '../../hooks/useIntersectionObserver';
+import { Membership } from '../../../types/matrix/room';
+import { UseStateProvider } from '../../components/UseStateProvider';
+
+export const MembershipFilters = {
+  filterJoined: (m: RoomMember) => m.membership === Membership.Join,
+  filterInvited: (m: RoomMember) => m.membership === Membership.Invite,
+  filterLeaved: (m: RoomMember) =>
+    m.membership === Membership.Leave &&
+    m.events.member?.getStateKey() === m.events.member?.getSender(),
+  filterKicked: (m: RoomMember) =>
+    m.membership === Membership.Leave &&
+    m.events.member?.getStateKey() !== m.events.member?.getSender(),
+  filterBanned: (m: RoomMember) => m.membership === Membership.Ban,
+};
+
+export type MembershipFilterFn = (m: RoomMember) => boolean;
+
+export type MembershipFilter = {
+  name: string;
+  filterFn: MembershipFilterFn;
+  color: ContainerColor;
+};
+
+const useMembershipFilterMenu = (): MembershipFilter[] =>
+  useMemo(
+    () => [
+      {
+        name: 'Joined',
+        filterFn: MembershipFilters.filterJoined,
+        color: 'Primary',
+      },
+      {
+        name: 'Invited',
+        filterFn: MembershipFilters.filterInvited,
+        color: 'Success',
+      },
+      {
+        name: 'Left',
+        filterFn: MembershipFilters.filterLeaved,
+        color: 'Secondary',
+      },
+      {
+        name: 'Kicked',
+        filterFn: MembershipFilters.filterKicked,
+        color: 'Warning',
+      },
+      {
+        name: 'Banned',
+        filterFn: MembershipFilters.filterBanned,
+        color: 'Critical',
+      },
+    ],
+    []
+  );
+
+export const SortFilters = {
+  filterAscending: (a: RoomMember, b: RoomMember) =>
+    a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1,
+  filterDescending: (a: RoomMember, b: RoomMember) =>
+    a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 1,
+  filterNewestFirst: (a: RoomMember, b: RoomMember) =>
+    (b.events.member?.getTs() ?? 0) - (a.events.member?.getTs() ?? 0),
+  filterOldest: (a: RoomMember, b: RoomMember) =>
+    (a.events.member?.getTs() ?? 0) - (b.events.member?.getTs() ?? 0),
+};
+
+export type SortFilterFn = (a: RoomMember, b: RoomMember) => number;
+
+export type SortFilter = {
+  name: string;
+  filterFn: SortFilterFn;
+};
+
+const useSortFilterMenu = (): SortFilter[] =>
+  useMemo(
+    () => [
+      {
+        name: 'Ascending',
+        filterFn: SortFilters.filterAscending,
+      },
+      {
+        name: 'Descending',
+        filterFn: SortFilters.filterDescending,
+      },
+      {
+        name: 'Newest First',
+        filterFn: SortFilters.filterNewestFirst,
+      },
+      {
+        name: 'Oldest First',
+        filterFn: SortFilters.filterOldest,
+      },
+    ],
+    []
+  );
+
+export type MembersFilterOptions = {
+  membershipFilter: MembershipFilter;
+  sortFilter: SortFilter;
+};
 
 type MembersDrawerProps = {
   room: Room;
@@ -35,10 +140,22 @@ export function MembersDrawer({ room }: MembersDrawerProps) {
   const scrollRef = useRef<HTMLDivElement>(null);
   const filterOptionsRef = useRef<HTMLDivElement>(null);
   const members = useRoomMembers(mx, room.roomId);
+
+  const membershipFilterMenu = useMembershipFilterMenu();
+  const sortFilterMenu = useSortFilterMenu();
+  const [filter, setFilter] = useState<MembersFilterOptions>({
+    membershipFilter: membershipFilterMenu[0],
+    sortFilter: sortFilterMenu[0],
+  });
   const [onTop, setOnTop] = useState(true);
 
+  const filteredMembers: RoomMember[] = useMemo(
+    () => members.filter(filter.membershipFilter.filterFn).sort(filter.sortFilter.filterFn),
+    [members, filter]
+  );
+
   const virtualizer = useVirtualizer({
-    count: members.length,
+    count: filteredMembers.length,
     getScrollElement: () => scrollRef.current,
     estimateSize: () => 40,
     overscan: 10,
@@ -53,11 +170,6 @@ export function MembersDrawer({ room }: MembersDrawerProps) {
     useCallback(() => ({ root: scrollRef.current }), []),
     useCallback(() => filterOptionsRef.current, [])
   );
-
-  // const groupedMembers: RoomMember[] = useMemo(() => {
-  //   const sorted
-  //   const m: RoomMember[] = [];
-  // }, [members]);
 
   return (
     <Box className={css.MembersDrawer} direction="Column">
@@ -85,23 +197,110 @@ export function MembersDrawer({ room }: MembersDrawerProps) {
 
             <Box ref={filterOptionsRef} className={css.DrawerGroup} direction="Column" gap="100">
               <Text size="L400">Filter</Text>
-              <Box alignItems="Center" gap="100">
-                <Chip
-                  variant="Primary"
-                  radii="Pill"
-                  outlined
-                  after={<Icon src={Icons.ChevronBottom} size="50" />}
-                >
-                  <Text size="T200">Joined</Text>
-                </Chip>
-                <Chip
-                  variant="Surface"
-                  radii="Pill"
-                  outlined
-                  after={<Icon src={Icons.ChevronBottom} size="50" />}
-                >
-                  <Text size="T200">Sort</Text>
-                </Chip>
+              <Box alignItems="Center" gap="100" wrap="Wrap">
+                <UseStateProvider initial={false}>
+                  {(open, setOpen) => (
+                    <PopOut
+                      open={open}
+                      position="Bottom"
+                      align="Start"
+                      content={
+                        <FocusTrap
+                          focusTrapOptions={{
+                            initialFocus: false,
+                            onDeactivate: () => setOpen(false),
+                            clickOutsideDeactivates: true,
+                            isKeyForward: (evt: KeyboardEvent) => evt.key === 'ArrowDown',
+                            isKeyBackward: (evt: KeyboardEvent) => evt.key === 'ArrowUp',
+                          }}
+                        >
+                          <Menu style={{ padding: config.space.S100 }}>
+                            {membershipFilterMenu.map((menuItem) => (
+                              <MenuItem
+                                key={menuItem.name}
+                                variant={
+                                  menuItem.name === filter.membershipFilter.name
+                                    ? menuItem.color
+                                    : 'Surface'
+                                }
+                                radii="300"
+                                onClick={() => {
+                                  setFilter((f) => ({ ...f, membershipFilter: menuItem }));
+                                  setOpen(false);
+                                }}
+                              >
+                                <Text>{menuItem.name}</Text>
+                              </MenuItem>
+                            ))}
+                          </Menu>
+                        </FocusTrap>
+                      }
+                    >
+                      {(anchorRef) => (
+                        <Chip
+                          ref={anchorRef}
+                          onClick={() => setOpen(!open)}
+                          variant={filter.membershipFilter.color}
+                          radii="Pill"
+                          outlined
+                          after={<Icon src={Icons.ChevronBottom} size="50" />}
+                        >
+                          <Text size="T200">{filter.membershipFilter.name}</Text>
+                        </Chip>
+                      )}
+                    </PopOut>
+                  )}
+                </UseStateProvider>
+                <UseStateProvider initial={false}>
+                  {(open, setOpen) => (
+                    <PopOut
+                      open={open}
+                      position="Bottom"
+                      align="Start"
+                      content={
+                        <FocusTrap
+                          focusTrapOptions={{
+                            initialFocus: false,
+                            onDeactivate: () => setOpen(false),
+                            clickOutsideDeactivates: true,
+                            isKeyForward: (evt: KeyboardEvent) => evt.key === 'ArrowDown',
+                            isKeyBackward: (evt: KeyboardEvent) => evt.key === 'ArrowUp',
+                          }}
+                        >
+                          <Menu style={{ padding: config.space.S100 }}>
+                            {sortFilterMenu.map((menuItem) => (
+                              <MenuItem
+                                key={menuItem.name}
+                                variant="Surface"
+                                aria-pressed={menuItem.name === filter.sortFilter.name}
+                                radii="300"
+                                onClick={() => {
+                                  setFilter((f) => ({ ...f, sortFilter: menuItem }));
+                                  setOpen(false);
+                                }}
+                              >
+                                <Text>{menuItem.name}</Text>
+                              </MenuItem>
+                            ))}
+                          </Menu>
+                        </FocusTrap>
+                      }
+                    >
+                      {(anchorRef) => (
+                        <Chip
+                          ref={anchorRef}
+                          onClick={() => setOpen(!open)}
+                          variant="Surface"
+                          radii="Pill"
+                          outlined
+                          after={<Icon src={Icons.ChevronBottom} size="50" />}
+                        >
+                          <Text size="T200">{`Order: ${filter.sortFilter.name}`}</Text>
+                        </Chip>
+                      )}
+                    </PopOut>
+                  )}
+                </UseStateProvider>
               </Box>
             </Box>
 
@@ -131,7 +330,7 @@ export function MembersDrawer({ room }: MembersDrawerProps) {
                 }}
               >
                 {virtualizer.getVirtualItems().map((vItem) => {
-                  const member = members[vItem.index];
+                  const member = filteredMembers[vItem.index];
                   const avatarUrl = member.getAvatarUrl(
                     mx.baseUrl,
                     100,
@@ -153,7 +352,7 @@ export function MembersDrawer({ room }: MembersDrawerProps) {
                       }}
                       data-index={vItem.index}
                       ref={virtualizer.measureElement}
-                      key={`${room.roomId}-${vItem.key}`}
+                      key={`${room.roomId}-${member.userId}`}
                       alignItems="Center"
                       gap="200"
                     >
