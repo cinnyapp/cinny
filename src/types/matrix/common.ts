f9f4a3654e4464d9d7fbbd7ba0c5af743fc74a64
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-08-17 12:01:33 +0530

    add attachment content types

--- src/types/matrix/common.ts
+++ src/types/matrix/common.ts
@@ -1,4 +1,5 @@
 import { EncryptedAttachmentInfo } from 'browser-encrypt-attachment';
+import { MsgType } from 'matrix-js-sdk';
 
 export type IImageInfo = {
   w?: number;
@@ -11,6 +12,17 @@ export type IVideoInfo = IImageInfo & {
   duration?: number;
 };
 
+export type IAudioInfo = {
+  mimetype?: string;
+  size?: string;
+  duration?: number;
+};
+
+export type IFileInfo = {
+  mimetype?: string;
+  size?: string;
+};
+
 export type IEncryptedFile = EncryptedAttachmentInfo & {
   url: string;
 };
@@ -19,4 +31,43 @@ export type IThumbnailContent = {
   thumbnail_info?: IImageInfo;
   thumbnail_file?: IEncryptedFile;
   thumbnail_url?: string;
+};
+
+export type IImageContent = {
+  msgtype: MsgType.Image;
+  body?: string;
+  url?: string;
+  info?: IImageInfo & IThumbnailContent;
+  file?: IEncryptedFile;
+};
+
+export type IVideoContent = {
+  msgtype: MsgType.Video;
+  body?: string;
+  url?: string;
+  info?: IVideoInfo & IThumbnailContent;
+  file?: IEncryptedFile;
+};
+
+export type IAudioContent = {
+  msgtype: MsgType.Audio;
+  body?: string;
+  url?: string;
+  info?: IAudioInfo;
+  file?: IEncryptedFile;
+};
+
+export type IFileContent = {
+  msgtype: MsgType.File;
+  body?: string;
+  url?: string;
+  info?: IFileInfo & IThumbnailContent;
+  file?: IEncryptedFile;
+};
+
+export type ILocationContent = {
+  msgtype: MsgType.Location;
+  body?: string;
+  geo_uri?: string;
+  info?: IThumbnailContent;
 };
