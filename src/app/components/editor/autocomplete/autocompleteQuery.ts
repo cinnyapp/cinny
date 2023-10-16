35841b954d0106a4157321719c16953edb2b7cbf
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-10-16 21:52:59 +0530

    add command auto complete menu

--- src/app/components/editor/autocomplete/autocompleteQuery.ts
+++ src/app/components/editor/autocomplete/autocompleteQuery.ts
@@ -4,11 +4,13 @@ export enum AutocompletePrefix {
   RoomMention = '#',
   UserMention = '@',
   Emoticon = ':',
+  Command = '/',
 }
 export const AUTOCOMPLETE_PREFIXES: readonly AutocompletePrefix[] = [
   AutocompletePrefix.RoomMention,
   AutocompletePrefix.UserMention,
   AutocompletePrefix.Emoticon,
+  AutocompletePrefix.Command,
 ];
 
 export type AutocompleteQuery<TPrefix extends string> = {
