1d6d825581ec4d459757c81f42677abe73290ac3
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-10-17 14:37:18 +0530

    test ipad user agent

--- src/app/utils/user-agent.ts
+++ src/app/utils/user-agent.ts
@@ -4,4 +4,10 @@ export const ua = () => UAParser(window.navigator.userAgent);
 
 export const isMacOS = () => ua().os.name === 'Mac OS';
 
-export const isAndroidOrIOS = (): boolean => ua().os.name === 'Android' || ua().os.name === 'iOS';
+export const isAndroidOrIOS = (): boolean => {
+  console.log('=========>');
+  console.log(ua());
+  console.log(ua().os, ua().os.name);
+  console.log('<========');
+  return ua().os.name === 'Android' || ua().os.name === 'iOS' || ua().os.name === 'iPadOS';
+};
