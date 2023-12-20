b51b2cc4e22b90d75fcb9aed8c4bb46152e4899b
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2023-12-20 15:12:52 +0530

    add promise utils

--- src/app/utils/common.ts
+++ src/app/utils/common.ts
@@ -44,6 +44,17 @@ export const fulfilledPromiseSettledResult = <T>(prs: PromiseSettledResult<T>[])
     return values;
   }, []);
 
+export const promiseFulfilledResult = <T>(
+  settledResult: PromiseSettledResult<T>
+): T | undefined => {
+  if (settledResult.status === 'fulfilled') return settledResult.value;
+  return undefined;
+};
+export const promiseRejectedResult = <T>(settledResult: PromiseSettledResult<T>): any => {
+  if (settledResult.status === 'rejected') return settledResult.reason;
+  return undefined;
+};
+
 export const binarySearch = <T>(items: T[], match: (item: T) => -1 | 0 | 1): T | undefined => {
   const search = (start: number, end: number): T | undefined => {
     if (start > end) return undefined;
