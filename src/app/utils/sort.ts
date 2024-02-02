b1a2d7a99d3e25e7356c2fb611cc1adb2d034c30
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2024-02-02 15:26:29 +0530

    add sort utils

---
+++ src/app/utils/sort.ts
@@ -0,0 +1,32 @@
+import { MatrixClient } from 'matrix-js-sdk';
+
+export type SortFunc<T> = (a: T, b: T) => number;
+
+export const factoryRoomIdByActivity =
+  (mx: MatrixClient): SortFunc<string> =>
+  (a, b) => {
+    const room1 = mx.getRoom(a);
+    const room2 = mx.getRoom(b);
+
+    return (room2?.getLastActiveTimestamp() ?? 0) - (room1?.getLastActiveTimestamp() ?? 0);
+  };
+
+export const factoryRoomIdByAtoZ =
+  (mx: MatrixClient): SortFunc<string> =>
+  (a, b) => {
+    let aName = mx.getRoom(a)?.name ?? '';
+    let bName = mx.getRoom(b)?.name ?? '';
+
+    // remove "#" from the room name
+    // To ignore it in sorting
+    aName = aName.replace(/#/g, '');
+    bName = bName.replace(/#/g, '');
+
+    if (aName.toLowerCase() < bName.toLowerCase()) {
+      return -1;
+    }
+    if (aName.toLowerCase() > bName.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  };
