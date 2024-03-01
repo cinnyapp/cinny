2b2fd606e194ba2a918cc8b25ed5f33654f16e5d
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2024-03-01 20:09:19 +0530

    use content component in featured community content

--- src/app/pages/client/explore/Featured.tsx
+++ src/app/pages/client/explore/Featured.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
-import { Box, Icon, Icons, Scroll, Text, config, toRem } from 'folds';
+import { Box, Icon, Icons, Text } from 'folds';
 import { useAtomValue } from 'jotai';
-import { ContainerColor } from '../../../styles/ContainerColor.css';
 import { useClientConfig } from '../../../hooks/useClientConfig';
 import { RoomCard } from '../../../components/room-card';
 import { getCanonicalAliasRoomId, isRoomAlias } from '../../../utils/matrix';
@@ -9,6 +8,7 @@ import * as css from './style.css';
 import { useMatrixClient } from '../../../hooks/useMatrixClient';
 import { allRoomsAtom } from '../../../state/room-list/roomList';
 import { RoomSummaryLoader } from '../../../components/RoomSummaryLoader';
+import { Content, ContentBody, ContentHero, ContentHeroSection } from '../../../components/content';
 
 export function FeaturedRooms() {
   const mx = useMatrixClient();
@@ -30,78 +30,56 @@ export function FeaturedRooms() {
   );
 
   return (
-    <Box grow="Yes" className={ContainerColor({ variant: 'Surface' })}>
-      <Scroll hideTrack>
-        <div
-          style={{
-            paddingLeft: config.space.S500,
-            paddingRight: config.space.S100,
-            paddingBottom: config.space.S700,
-          }}
-        >
-          <div style={{ padding: '40px 16px' }}>
-            <Box direction="Column" gap="400">
-              <Box direction="Column" alignItems="Center" gap="200">
-                <Icon size="600" src={Icons.Bulb} />
-              </Box>
-              <Box direction="Column" gap="100" alignItems="Center">
-                <Text size="H2">Featured by Client</Text>
-                <Text priority="400">
-                  Find and explore public rooms and spaces featured by client provider.
-                </Text>
-              </Box>
-            </Box>
-          </div>
-          <Box
-            direction="Column"
-            gap="500"
-            style={{
-              maxWidth: toRem(848),
-              margin: 'auto',
-            }}
-          >
-            <Box direction="Column" gap="400">
-              <Text size="H4">Featured Spaces</Text>
-              <Box className={css.CardGrid} gap="400" wrap="Wrap">
-                {spaces?.map((roomIdOrAlias) => (
-                  <RoomSummaryLoader key={roomIdOrAlias} roomIdOrAlias={roomIdOrAlias}>
-                    {(roomSummary) => (
-                      <RoomCard
-                        roomIdOrAlias={roomIdOrAlias}
-                        joined={joinedRoom(roomIdOrAlias)}
-                        avatarUrl={roomSummary?.avatar_url}
-                        name={roomSummary?.name}
-                        topic={roomSummary?.topic}
-                        memberCount={roomSummary?.num_joined_members}
-                      />
-                    )}
-                  </RoomSummaryLoader>
-                ))}
-              </Box>
-            </Box>
-
-            <Box direction="Column" gap="400">
-              <Text size="H4">Featured Rooms</Text>
-              <Box className={css.CardGrid} gap="400" wrap="Wrap">
-                {rooms?.map((roomIdOrAlias) => (
-                  <RoomSummaryLoader key={roomIdOrAlias} roomIdOrAlias={roomIdOrAlias}>
-                    {(roomSummary) => (
-                      <RoomCard
-                        roomIdOrAlias={roomIdOrAlias}
-                        joined={joinedRoom(roomIdOrAlias)}
-                        avatarUrl={roomSummary?.avatar_url}
-                        name={roomSummary?.name}
-                        topic={roomSummary?.topic}
-                        memberCount={roomSummary?.num_joined_members}
-                      />
-                    )}
-                  </RoomSummaryLoader>
-                ))}
-              </Box>
+    <Content>
+      <ContentHeroSection>
+        <ContentHero
+          icon={<Icon size="600" src={Icons.Bulb} />}
+          title="Featured by Client"
+          subTitle="Find and explore public rooms and spaces featured by client provider."
+        />
+      </ContentHeroSection>
+      <ContentBody>
+        <Box direction="Column" gap="700">
+          <Box direction="Column" gap="400">
+            <Text size="H4">Featured Spaces</Text>
+            <Box className={css.CardGrid} gap="400" wrap="Wrap">
+              {spaces?.map((roomIdOrAlias) => (
+                <RoomSummaryLoader key={roomIdOrAlias} roomIdOrAlias={roomIdOrAlias}>
+                  {(roomSummary) => (
+                    <RoomCard
+                      roomIdOrAlias={roomIdOrAlias}
+                      joined={joinedRoom(roomIdOrAlias)}
+                      avatarUrl={roomSummary?.avatar_url}
+                      name={roomSummary?.name}
+                      topic={roomSummary?.topic}
+                      memberCount={roomSummary?.num_joined_members}
+                    />
+                  )}
+                </RoomSummaryLoader>
+              ))}
             </Box>
           </Box>
-        </div>
-      </Scroll>
-    </Box>
+          <Box direction="Column" gap="400">
+            <Text size="H4">Featured Rooms</Text>
+            <Box className={css.CardGrid} gap="400" wrap="Wrap">
+              {rooms?.map((roomIdOrAlias) => (
+                <RoomSummaryLoader key={roomIdOrAlias} roomIdOrAlias={roomIdOrAlias}>
+                  {(roomSummary) => (
+                    <RoomCard
+                      roomIdOrAlias={roomIdOrAlias}
+                      joined={joinedRoom(roomIdOrAlias)}
+                      avatarUrl={roomSummary?.avatar_url}
+                      name={roomSummary?.name}
+                      topic={roomSummary?.topic}
+                      memberCount={roomSummary?.num_joined_members}
+                    />
+                  )}
+                </RoomSummaryLoader>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+      </ContentBody>
+    </Content>
   );
 }
