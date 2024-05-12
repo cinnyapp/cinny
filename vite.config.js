c3f0483432816ce21ff182e095f717554d6aa8b1
Krishan <33421343+kfiven@users.noreply.github.com>
2024-05-12 14:05:00 +1000

    Fix pdfjs top level await

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { wasm } from '@rollup/plugin-wasm';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import inject from '@rollup/plugin-inject';
-import { svgLoader } from './viteSvgLoader'
-import buildConfig from "./build.config"
+import topLevelAwait from 'vite-plugin-top-level-await';
+import { svgLoader } from './viteSvgLoader';
+import buildConfig from './build.config';
 
 const copyFiles = {
   targets: [
@@ -35,7 +36,7 @@ const copyFiles = {
       dest: 'public/',
     },
   ],
-}
+};
 
 export default defineConfig({
   appType: 'spa',
@@ -46,6 +47,12 @@ export default defineConfig({
     host: true,
   },
   plugins: [
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
     viteStaticCopy(copyFiles),
     vanillaExtractPlugin(),
     svgLoader(),
@@ -54,26 +61,24 @@ export default defineConfig({
   ],
   optimizeDeps: {
     esbuildOptions: {
-        define: {
-          global: 'globalThis'
-        },
-        plugins: [
-          // Enable esbuild polyfill plugins
-          NodeGlobalsPolyfillPlugin({
-            process: false,
-            buffer: true,
-          }),
-        ]
-    }
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        // Enable esbuild polyfill plugins
+        NodeGlobalsPolyfillPlugin({
+          process: false,
+          buffer: true,
+        }),
+      ],
+    },
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
     copyPublicDir: false,
     rollupOptions: {
-      plugins: [
-        inject({ Buffer: ['buffer', 'Buffer'] })
-      ]
-    }
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+    },
   },
 });
