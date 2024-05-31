57b92b1f704807bb6a5904ef6a2154f1ab939865
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2024-05-31 11:16:27 +0530

    add context menu for direct and home tab

--- src/app/pages/client/sidebar/DirectTab.tsx
+++ src/app/pages/client/sidebar/DirectTab.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { MouseEventHandler, forwardRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Icon, Icons } from 'folds';
+import { Box, Icon, Icons, Menu, MenuItem, PopOut, RectCords, Text, config, toRem } from 'folds';
+import FocusTrap from 'focus-trap-react';
 import { useAtomValue } from 'jotai';
 import { useDirects } from '../../../state/hooks/roomList';
 import { useMatrixClient } from '../../../hooks/useMatrixClient';
@@ -19,6 +20,40 @@ import { useDirectSelected } from '../../../hooks/router/useDirectSelected';
 import { UnreadBadge } from '../../../components/unread-badge';
 import { ScreenSize, useScreenSizeContext } from '../../../hooks/useScreenSize';
 import { useNavToActivePathAtom } from '../../../state/hooks/navToActivePath';
+import { useDirectRooms } from '../direct/useDirectRooms';
+import { markAsRead } from '../../../../client/action/notifications';
+
+type DirectMenuProps = {
+  requestClose: () => void;
+};
+const DirectMenu = forwardRef<HTMLDivElement, DirectMenuProps>(({ requestClose }, ref) => {
+  const orphanRooms = useDirectRooms();
+  const unread = useRoomsUnread(orphanRooms, roomToUnreadAtom);
+
+  const handleMarkAsRead = () => {
+    if (!unread) return;
+    orphanRooms.forEach((rId) => markAsRead(rId));
+    requestClose();
+  };
+
+  return (
+    <Menu ref={ref} style={{ maxWidth: toRem(160), width: '100vw' }}>
+      <Box direction="Column" gap="100" style={{ padding: config.space.S100 }}>
+        <MenuItem
+          onClick={handleMarkAsRead}
+          size="300"
+          after={<Icon size="100" src={Icons.CheckTwice} />}
+          radii="300"
+          aria-disabled={!unread}
+        >
+          <Text style={{ flexGrow: 1 }} as="span" size="T300" truncate>
+            Mark as Read
+          </Text>
+        </MenuItem>
+      </Box>
+    </Menu>
+  );
+});
 
 export function DirectTab() {
   const navigate = useNavigate();
@@ -29,6 +64,7 @@ export function DirectTab() {
   const mDirects = useAtomValue(mDirectAtom);
   const directs = useDirects(mx, allRoomsAtom, mDirects);
   const directUnread = useRoomsUnread(directs, roomToUnreadAtom);
+  const [menuAnchor, setMenuAnchor] = useState<RectCords>();
 
   const directSelected = useDirectSelected();
 
@@ -42,11 +78,25 @@ export function DirectTab() {
     navigate(getDirectPath());
   };
 
+  const handleContextMenu: MouseEventHandler<HTMLButtonElement> = (evt) => {
+    evt.preventDefault();
+    const cords = evt.currentTarget.getBoundingClientRect();
+    setMenuAnchor((currentState) => {
+      if (currentState) return undefined;
+      return cords;
+    });
+  };
   return (
     <SidebarItem active={directSelected}>
       <SidebarItemTooltip tooltip="Direct Messages">
         {(triggerRef) => (
-          <SidebarAvatar as="button" ref={triggerRef} outlined onClick={handleDirectClick}>
+          <SidebarAvatar
+            as="button"
+            ref={triggerRef}
+            outlined
+            onClick={handleDirectClick}
+            onContextMenu={handleContextMenu}
+          >
             <Icon src={Icons.User} filled={directSelected} />
           </SidebarAvatar>
         )}
@@ -55,6 +105,27 @@ export function DirectTab() {
         <SidebarItemBadge hasCount={directUnread.total > 0}>
           <UnreadBadge highlight={directUnread.highlight > 0} count={directUnread.total} />
         </SidebarItemBadge>
+      )}
+      {menuAnchor && (
+        <PopOut
+          anchor={menuAnchor}
+          position="Right"
+          align="Start"
+          content={
+            <FocusTrap
+              focusTrapOptions={{
+                initialFocus: false,
+                returnFocusOnDeactivate: false,
+                onDeactivate: () => setMenuAnchor(undefined),
+                clickOutsideDeactivates: true,
+                isKeyForward: (evt: KeyboardEvent) => evt.key === 'ArrowDown',
+                isKeyBackward: (evt: KeyboardEvent) => evt.key === 'ArrowUp',
+              }}
+            >
+              <DirectMenu requestClose={() => setMenuAnchor(undefined)} />
+            </FocusTrap>
+          }
+        />
       )}
     </SidebarItem>
   );
--- src/app/pages/client/sidebar/HomeTab.tsx
+++ src/app/pages/client/sidebar/HomeTab.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { MouseEventHandler, forwardRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Icon, Icons } from 'folds';
+import { Box, Icon, Icons, Menu, MenuItem, PopOut, RectCords, Text, config, toRem } from 'folds';
 import { useAtomValue } from 'jotai';
+import FocusTrap from 'focus-trap-react';
 import { useOrphanRooms } from '../../../state/hooks/roomList';
 import { useMatrixClient } from '../../../hooks/useMatrixClient';
 import { mDirectAtom } from '../../../state/mDirectList';
@@ -20,6 +21,40 @@ import { useHomeSelected } from '../../../hooks/router/useHomeSelected';
 import { UnreadBadge } from '../../../components/unread-badge';
 import { ScreenSize, useScreenSizeContext } from '../../../hooks/useScreenSize';
 import { useNavToActivePathAtom } from '../../../state/hooks/navToActivePath';
+import { useHomeRooms } from '../home/useHomeRooms';
+import { markAsRead } from '../../../../client/action/notifications';
+
+type HomeMenuProps = {
+  requestClose: () => void;
+};
+const HomeMenu = forwardRef<HTMLDivElement, HomeMenuProps>(({ requestClose }, ref) => {
+  const orphanRooms = useHomeRooms();
+  const unread = useRoomsUnread(orphanRooms, roomToUnreadAtom);
+
+  const handleMarkAsRead = () => {
+    if (!unread) return;
+    orphanRooms.forEach((rId) => markAsRead(rId));
+    requestClose();
+  };
+
+  return (
+    <Menu ref={ref} style={{ maxWidth: toRem(160), width: '100vw' }}>
+      <Box direction="Column" gap="100" style={{ padding: config.space.S100 }}>
+        <MenuItem
+          onClick={handleMarkAsRead}
+          size="300"
+          after={<Icon size="100" src={Icons.CheckTwice} />}
+          radii="300"
+          aria-disabled={!unread}
+        >
+          <Text style={{ flexGrow: 1 }} as="span" size="T300" truncate>
+            Mark as Read
+          </Text>
+        </MenuItem>
+      </Box>
+    </Menu>
+  );
+});
 
 export function HomeTab() {
   const navigate = useNavigate();
@@ -32,6 +67,7 @@ export function HomeTab() {
   const orphanRooms = useOrphanRooms(mx, allRoomsAtom, mDirects, roomToParents);
   const homeUnread = useRoomsUnread(orphanRooms, roomToUnreadAtom);
   const homeSelected = useHomeSelected();
+  const [menuAnchor, setMenuAnchor] = useState<RectCords>();
 
   const handleHomeClick = () => {
     const activePath = navToActivePath.get('home');
@@ -43,11 +79,26 @@ export function HomeTab() {
     navigate(getHomePath());
   };
 
+  const handleContextMenu: MouseEventHandler<HTMLButtonElement> = (evt) => {
+    evt.preventDefault();
+    const cords = evt.currentTarget.getBoundingClientRect();
+    setMenuAnchor((currentState) => {
+      if (currentState) return undefined;
+      return cords;
+    });
+  };
+
   return (
     <SidebarItem active={homeSelected}>
       <SidebarItemTooltip tooltip="Home">
         {(triggerRef) => (
-          <SidebarAvatar as="button" ref={triggerRef} outlined onClick={handleHomeClick}>
+          <SidebarAvatar
+            as="button"
+            ref={triggerRef}
+            outlined
+            onClick={handleHomeClick}
+            onContextMenu={handleContextMenu}
+          >
             <Icon src={Icons.Home} filled={homeSelected} />
           </SidebarAvatar>
         )}
@@ -56,6 +107,27 @@ export function HomeTab() {
         <SidebarItemBadge hasCount={homeUnread.total > 0}>
           <UnreadBadge highlight={homeUnread.highlight > 0} count={homeUnread.total} />
         </SidebarItemBadge>
+      )}
+      {menuAnchor && (
+        <PopOut
+          anchor={menuAnchor}
+          position="Right"
+          align="Start"
+          content={
+            <FocusTrap
+              focusTrapOptions={{
+                initialFocus: false,
+                returnFocusOnDeactivate: false,
+                onDeactivate: () => setMenuAnchor(undefined),
+                clickOutsideDeactivates: true,
+                isKeyForward: (evt: KeyboardEvent) => evt.key === 'ArrowDown',
+                isKeyBackward: (evt: KeyboardEvent) => evt.key === 'ArrowUp',
+              }}
+            >
+              <HomeMenu requestClose={() => setMenuAnchor(undefined)} />
+            </FocusTrap>
+          }
+        />
       )}
     </SidebarItem>
   );
