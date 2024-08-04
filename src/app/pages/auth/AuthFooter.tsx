e68c56b334107862ab2eec5635c11c40c050f00b
Krishan <33421343+kfiven@users.noreply.github.com>
2024-08-04 20:15:10 +1000

    Release v4.1.0 (#1867)

--- src/app/pages/auth/AuthFooter.tsx
+++ src/app/pages/auth/AuthFooter.tsx
@@ -15,7 +15,7 @@ export function AuthFooter() {
         target="_blank"
         rel="noreferrer"
       >
-        v4.0.3
+        v4.1.0
       </Text>
       <Text as="a" size="T300" href="https://twitter.com/cinnyapp" target="_blank" rel="noreferrer">
         Twitter
