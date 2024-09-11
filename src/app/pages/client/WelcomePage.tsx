c75e90361928c18cf33893954c50ce4600e0356a
Krishan <33421343+kfiven@users.noreply.github.com>
2024-09-11 23:56:08 +1000

    Release v4.2.0 (#1949)

--- src/app/pages/client/WelcomePage.tsx
+++ src/app/pages/client/WelcomePage.tsx
@@ -24,7 +24,7 @@ export function WelcomePage() {
                   target="_blank"
                   rel="noreferrer noopener"
                 >
-                  v4.1.0
+                  v4.2.0
                 </a>
               </span>
             }
