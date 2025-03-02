e0c168705c57d5f85bbfeaea5e6ce80f1a931374
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2025-03-02 19:26:10 +0100

    Fix image paste.

--- src/app/hooks/useFilePasteHandler.ts
+++ src/app/hooks/useFilePasteHandler.ts
@@ -1,11 +1,33 @@
 import { useCallback, ClipboardEventHandler } from 'react';
 import { getDataTransferFiles } from '../utils/dom';
+import {invoke} from '@tauri-apps/api';
+
+function base64ToFile(base64: string, filename: string, type: string): File {
+  const binary = atob(base64)
+  let n = binary.length
+  const u8arr = new Uint8Array(n);
+  while(n--){
+    u8arr[n] = binary.charCodeAt(n);
+  }
+  return new File([u8arr], filename, {type});
+}
 
 export const useFilePasteHandler = (onPaste: (file: File[]) => void): ClipboardEventHandler =>
   useCallback(
-    (evt) => {
-      const files = getDataTransferFiles(evt.clipboardData);
-      if (files) onPaste(files);
+    async (evt) => {
+      if (window.__TAURI__) {
+        try {
+          const result: string = await invoke("clipboard_read_image");
+          const file = base64ToFile(result, 'image.png', 'image/png');
+          evt.preventDefault();
+          onPaste([file]);
+        } catch(e) {
+
+        }
+      } else {
+        const files = getDataTransferFiles(evt.clipboardData);
+        if (files) onPaste(files);
+      }
     },
     [onPaste]
   );
