20ab8e93df43040d68b43eb07615c970fb5f1207
LeaPhant <mail@lea.moe>
2025-04-19 12:23:17 +0200

    Move ImageOverlay props into extended type

--- src/app/components/ImageOverlay.tsx
+++ src/app/components/ImageOverlay.tsx
@@ -10,34 +10,36 @@ export type RenderViewerProps = {
   requestClose: () => void;
 };
 
-export const ImageOverlay = as<
-  'div',
-  {
-    src: string;
-    alt: string;
-    viewer: boolean;
-    requestClose: () => void;
-    renderViewer: (props: RenderViewerProps) => ReactNode;
-  }
->(({ src, alt, viewer, requestClose, renderViewer, ...props }, ref) => (
-  <Overlay {...props} ref={ref} open={viewer} backdrop={<OverlayBackdrop />}>
-    <OverlayCenter>
-      <FocusTrap
-        focusTrapOptions={{
-          initialFocus: false,
-          onDeactivate: () => requestClose(),
-          clickOutsideDeactivates: true,
-          escapeDeactivates: stopPropagation,
-        }}
-      >
-        <Modal className={ModalWide} size="500" onContextMenu={(evt: any) => evt.stopPropagation()}>
-          {renderViewer({
-            src,
-            alt,
-            requestClose: () => requestClose(),
-          })}
-        </Modal>
-      </FocusTrap>
-    </OverlayCenter>
-  </Overlay>
-));
+export type ImageOverlayProps = RenderViewerProps & {
+  viewer: boolean;
+  renderViewer: (props: RenderViewerProps) => ReactNode;
+};
+
+export const ImageOverlay = as<'div', ImageOverlayProps>(
+  ({ src, alt, viewer, requestClose, renderViewer, ...props }, ref) => (
+    <Overlay {...props} ref={ref} open={viewer} backdrop={<OverlayBackdrop />}>
+      <OverlayCenter>
+        <FocusTrap
+          focusTrapOptions={{
+            initialFocus: false,
+            onDeactivate: () => requestClose(),
+            clickOutsideDeactivates: true,
+            escapeDeactivates: stopPropagation,
+          }}
+        >
+          <Modal
+            className={ModalWide}
+            size="500"
+            onContextMenu={(evt: any) => evt.stopPropagation()}
+          >
+            {renderViewer({
+              src,
+              alt,
+              requestClose,
+            })}
+          </Modal>
+        </FocusTrap>
+      </OverlayCenter>
+    </Overlay>
+  )
+);
