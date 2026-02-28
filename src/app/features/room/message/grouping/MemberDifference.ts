32bc5d02796fb10f94779097b1a39bbe2ad566be
Sollace <sollacea@gmail.com>
2026-02-28 19:49:12 +0000

    Fix profile changes not being included in the summary

--- src/app/features/room/message/grouping/MemberDifference.ts
+++ src/app/features/room/message/grouping/MemberDifference.ts
@@ -15,6 +15,11 @@ export const createMemberChangeTracker = (room) => {
         members: {},
         verb: 'left'
     };
+    const profiles = {
+        count: 0,
+        members: {},
+        verb: 'changed their profile'
+    };
     const others = {
         count: 0,
         members: {}
@@ -37,29 +42,40 @@ export const createMemberChangeTracker = (room) => {
         return `${names[0]}, ${names[1]}, and ${counter.count - 2} others ${counter.verb}`;
     }
 
+    function getRelevantCounter(mEvent) {
+        const content = mEvent.getContent<IMemberContent>();
+        const prevContent = mEvent.getPrevContent() as IMemberContent;
+
+        if (!isMembershipChanged(mEvent)) {
+            if (content.displayname !== prevContent.displayname || content.avatar_url !== prevContent.avatar_url) {
+                return profiles;
+            }
+            return others;
+        }
+
+        return counters[content.membership] || others;
+    }
+
     return {
 
         accept(timelineEvent: TimelineEvent) {
             const mEvent = timelineEvent.mEvent;
-            if (isMembershipChanged(mEvent)) {
-                const content = mEvent.getContent<IMemberContent>();
-                const prevContent = mEvent.getPrevContent() as IMemberContent;
 
-                const senderId = mEvent.getSender() ?? '';
-                const senderName = getMemberDisplayName(room, senderId) || getMxIdLocalPart(senderId);
+            const senderId = mEvent.getSender() ?? '';
+            const senderName = getMemberDisplayName(room, senderId) || getMxIdLocalPart(senderId);
 
-                const counter = counters[content.membership] || others;
+            const counter = getRelevantCounter(mEvent);
 
-                if (!counter.members[senderName]) {
-                    counter.members[senderName] = true;
-                    counter.count++;
-                }
+            if (!counter.members[senderName]) {
+                counter.members[senderName] = true;
+                counter.count++;
             }
         },
         getFinalMessage() {
             return [
                 getCountMessage(joins),
                 getCountMessage(leaves),
+                getCountMessage(profiles),
                 (others.count > 0 ? ` and ${others.count} other events` : '')
             ].filter(m => !!m).join(', ');
         }
