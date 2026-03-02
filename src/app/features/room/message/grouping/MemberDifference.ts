81d25c472f8f6701c7e8cb4df0beea64ccb18b46
Sollace <sollacea@gmail.com>
2026-03-02 14:56:45 +0000

    Added case for members getting kicked

--- src/app/features/room/message/grouping/MemberDifference.ts
+++ src/app/features/room/message/grouping/MemberDifference.ts
@@ -20,6 +20,16 @@ export const createMemberChangeTracker = (room) => {
         members: {},
         verb: 'changed their profile'
     };
+    const kicks = {
+        count: 0,
+        members: {},
+        verb: 'kicked',
+        inverse: {
+            count: 0,
+            members: {},
+            verb: ''
+        }
+    };
     const others = {
         count: 0,
         members: {}
@@ -34,7 +44,7 @@ export const createMemberChangeTracker = (room) => {
         if (counter.count == 0) {
             return '';
         }
-        const names = Object.keys(counter.members);
+        const names = Object.keys(counter.members).map(id => getMemberDisplayName(room, id) || getMxIdLocalPart(id));
         if (counter.count <= 2) {
             return `${names.join(' and ')} ${counter.verb}`;
         }
@@ -53,6 +63,13 @@ export const createMemberChangeTracker = (room) => {
             return others;
         }
 
+        const userId = mEvent.getStateKey() ?? '';
+        const senderId = mEvent.getSender() ?? '';
+
+        if (userId !== senderId) {
+            return kicks;
+        }
+
         return counters[content.membership] || others;
     }
 
@@ -61,14 +78,22 @@ export const createMemberChangeTracker = (room) => {
         accept(timelineEvent: TimelineEvent) {
             const mEvent = timelineEvent.mEvent;
 
+            const userId = mEvent.getStateKey() ?? '';
             const senderId = mEvent.getSender() ?? '';
-            const senderName = getMemberDisplayName(room, senderId) || getMxIdLocalPart(senderId);
 
-            const counter = getRelevantCounter(mEvent);
+            let counter = getRelevantCounter(mEvent);
 
-            if (!counter.members[senderName]) {
-                counter.members[senderName] = true;
+            if (!counter.members[senderId]) {
+                counter.members[senderId] = true;
                 counter.count++;
+            }
+
+            if (counter.inverse) {
+                counter = counter.inverse;
+                if (!counter.members[userId]) {
+                    counter.members[userId] = true;
+                    counter.count++;
+                }
             }
         },
         getFinalMessage() {
@@ -76,6 +101,7 @@ export const createMemberChangeTracker = (room) => {
                 getCountMessage(joins),
                 getCountMessage(leaves),
                 getCountMessage(profiles),
+                (kicks.count > 0 && kicks.inverse.count > 0 ? `${getCountMessage(kicks)} ${getCountMessage(kicks.inverse)}` : ''),
                 (others.count > 0 ? ` and ${others.count} other events` : '')
             ].filter(m => !!m).join(', ');
         }
