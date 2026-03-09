ea018e9e88ae54485f5881006803d1b44f678a92
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2026-03-09 08:20:03 +0530

    hide call statusbar for mobile view when room is selected

--- src/app/pages/CallStatusRenderer.tsx
+++ src/app/pages/CallStatusRenderer.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useCallEmbed } from '../hooks/useCallEmbed';
 import { CallStatus } from '../features/call-status';
+import { useSelectedRoom } from '../hooks/router/useSelectedRoom';
+import { ScreenSize, useScreenSizeContext } from '../hooks/useScreenSize';
 
 export function CallStatusRenderer() {
   const callEmbed = useCallEmbed();
+  const selectedRoom = useSelectedRoom();
+
+  const screenSize = useScreenSizeContext();
 
   if (!callEmbed) return null;
+
+  if (screenSize === ScreenSize.Mobile && callEmbed.roomId === selectedRoom) return null;
 
   return <CallStatus callEmbed={callEmbed} />;
 }
