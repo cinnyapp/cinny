02bff2419b06eba9fbe8c1b1eca77597d3f039ba
Krishan <33421343+kfiven@users.noreply.github.com>
2026-03-11 04:07:37 +0000

    chore(release): 4.11.0 [skip ci]

--- src/app/pages/client/WelcomePage.tsx
+++ src/app/pages/client/WelcomePage.tsx
@@ -24,7 +24,7 @@ export function WelcomePage() {
                   target="_blank"
                   rel="noreferrer noopener"
                 >
-                  v4.10.5
+                  v4.11.0
                 </a>
               </span>
             }
