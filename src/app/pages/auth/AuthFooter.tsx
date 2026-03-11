b5bc77c308e6087a522031c50a13cd67fd144547
Krishan <33421343+kfiven@users.noreply.github.com>
2026-03-11 04:12:53 +0000

    chore(release): 4.11.1 [skip ci]

--- src/app/pages/auth/AuthFooter.tsx
+++ src/app/pages/auth/AuthFooter.tsx
@@ -15,7 +15,7 @@ export function AuthFooter() {
         target="_blank"
         rel="noreferrer"
       >
-        v4.11.0
+        v4.11.1
       </Text>
       <Text as="a" size="T300" href="https://twitter.com/cinnyapp" target="_blank" rel="noreferrer">
         Twitter
