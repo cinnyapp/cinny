b2d2b5c04dd6058d60a4a2d33f53407f68997d8e
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2026-03-15 15:37:14 +1100

    fix: prevent codeblock filename drop on edit (#2780)
    prevent codeblock filename drop on edit

--- src/app/plugins/markdown/block/rules.ts
+++ src/app/plugins/markdown/block/rules.ts
@@ -18,7 +18,7 @@ export const CodeBlockRule: BlockMDRule = {
     const [, g1, g2] = match;
     // use last identifier after dot, e.g. for "example.json" gets us "json" as language code.
     const langCode = g1 ? g1.substring(g1.lastIndexOf('.') + 1) : null;
-    const filename = g1 != langCode ? g1 : null;
+    const filename = g1 !== langCode ? g1 : null;
     const classNameAtt = langCode ? ` class="language-${langCode}"` : '';
     const filenameAtt = filename ? ` data-label="${filename}"` : '';
     return `<pre data-md="${CODEBLOCK_MD_1}"><code${classNameAtt}${filenameAtt}>${g2}</code></pre>`;
