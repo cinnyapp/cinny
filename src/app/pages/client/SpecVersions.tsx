f000d34a504626f5235b9552f06d9fa310da49c1
Krishan <33421343+kfiven@users.noreply.github.com>
2026-03-27 17:23:41 +1100

    chore: use similar wording

--- src/app/pages/client/SpecVersions.tsx
+++ src/app/pages/client/SpecVersions.tsx
@@ -22,7 +22,7 @@ export function SpecVersions({ baseUrl, children }: { baseUrl: string; children:
             <Dialog>
               <Box direction="Column" gap="400" style={{ padding: config.space.S400 }}>
                 <Text>
-                  Unable to connect to the homeserver. The server or your internet connection may be down.
+                  Unable to connect to the homeserver. The homeserver or your internet connection may be down.
                 </Text>
                 <Button variant="Critical" onClick={retry}>
                   <Text as="span" size="B400">
