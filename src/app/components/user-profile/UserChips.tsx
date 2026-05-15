da4f171ad28be6773071432644ae51d0dd9077e2
Ajay Bura <32841439+ajbura@users.noreply.github.com>
2026-05-15 11:45:39 +0530

    add support for stable mutual rooms endpoint

--- src/app/components/user-profile/UserChips.tsx
+++ src/app/components/user-profile/UserChips.tsx
@@ -28,7 +28,7 @@ import { copyToClipboard } from '../../utils/dom';
 import { getExploreServerPath } from '../../pages/pathUtils';
 import { AsyncStatus, useAsyncCallback } from '../../hooks/useAsyncCallback';
 import { factoryRoomIdByAtoZ } from '../../utils/sort';
-import { useMutualRooms, useMutualRoomsSupport } from '../../hooks/useMutualRooms';
+import { useMutualRooms } from '../../hooks/useMutualRooms';
 import { useRoomNavigate } from '../../hooks/useRoomNavigate';
 import { useDirectRooms } from '../../pages/client/direct/useDirectRooms';
 import { useMediaAuthentication } from '../../hooks/useMediaAuthentication';
@@ -232,8 +232,8 @@ type MutualRoomsData = {
 
 export function MutualRoomsChip({ userId }: { userId: string }) {
   const mx = useMatrixClient();
-  const mutualRoomSupported = useMutualRoomsSupport();
   const mutualRoomsState = useMutualRooms(userId);
+
   const { navigateRoom, navigateSpace } = useRoomNavigate();
   const closeUserRoomProfile = useCloseUserRoomProfile();
   const directs = useDirectRooms();
@@ -277,11 +277,7 @@ export function MutualRoomsChip({ userId }: { userId: string }) {
     return data;
   }, [mutualRoomsState, getRoom, directs, mx]);
 
-  if (
-    userId === mx.getSafeUserId() ||
-    !mutualRoomSupported ||
-    mutualRoomsState.status === AsyncStatus.Error
-  ) {
+  if (userId === mx.getSafeUserId() || mutualRoomsState.status === AsyncStatus.Error) {
     return null;
   }
 
--- src/app/hooks/useMutualRooms.ts
+++ src/app/hooks/useMutualRooms.ts
@@ -1,9 +1,10 @@
 import { useCallback } from 'react';
+import { MatrixClient, Method } from 'matrix-js-sdk';
 import { useMatrixClient } from './useMatrixClient';
 import { AsyncState, useAsyncCallbackValue } from './useAsyncCallback';
 import { useSpecVersions } from './useSpecVersions';
 
-export const useMutualRoomsSupport = (): boolean => {
+const useUnstableMutualRoomsSupport = (): boolean => {
   const { unstable_features: unstableFeatures } = useSpecVersions();
 
   const supported =
@@ -14,15 +15,47 @@ export const useMutualRoomsSupport = (): boolean => {
   return !!supported;
 };
 
+type MutualRoomsOK = {
+  joined: string[];
+  next_batch?: string;
+  count: number;
+};
+
+const fetchAllMutualRooms = async (mx: MatrixClient, userId: string): Promise<string[]> => {
+  const mutualRooms: Set<string> = new Set();
+
+  let nextBatch: string | undefined;
+  do {
+    // eslint-disable-next-line no-await-in-loop
+    const result = await mx.http.authedRequest<MutualRoomsOK>(
+      Method.Get,
+      '/mutual_rooms',
+      {
+        user_id: userId,
+        from: nextBatch,
+      },
+      undefined,
+      {
+        prefix: '/_matrix/client/v1',
+      }
+    );
+    result.joined.forEach((r) => mutualRooms.add(r));
+    nextBatch = result.next_batch;
+  } while (typeof nextBatch === 'string');
+
+  return Array.from(mutualRooms);
+};
+
 export const useMutualRooms = (userId: string): AsyncState<string[], unknown> => {
   const mx = useMatrixClient();
 
-  const supported = useMutualRoomsSupport();
+  const unstableSupport = useUnstableMutualRoomsSupport();
 
   const [mutualRoomsState] = useAsyncCallbackValue(
     useCallback(
-      () => (supported ? mx._unstable_getSharedRooms(userId) : Promise.resolve([])),
-      [mx, userId, supported]
+      () =>
+        unstableSupport ? mx._unstable_getSharedRooms(userId) : fetchAllMutualRooms(mx, userId),
+      [mx, userId, unstableSupport]
     )
   );
 
